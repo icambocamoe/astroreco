73efa369e7303cf30d82782bd794a1fd8b4c018e
MartinB04 <martin.bibiano4@outlook.com>
2024-11-13 10:07:31 -0600

    Add: DynamincAppTheme.js agregado
    * Se encarga de gestionar los styles dinámicos de los temas.

---
+++ src/theme/DynamicAppTheme.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native"
+import { ThemeContext, ThemeData } from "../Components/ThemeContext"
+import { useContext } from "react"
+
+
+export const dynamicStylesAppTheme = (themeData/* : ThemeData */) => {
+
+    return StyleSheet.create({
+        dynamicScrollViewStyle: {
+            backgroundColor: themeData.fondo,
+        },
+
+        dynamicMainContainer: {
+            backgroundColor: themeData.contenedorPrincipal,
+        },
+
+        dynamicViewContainer: {
+            backgroundColor: themeData.vistas,
+        },
+        dynamicText: {
+            color: themeData.texto,
+        },
+        dynamicBorder: {
+            borderColor: themeData.texto,
+            borderWidth: 1,
+        },
+        dynamicButton: {
+            backgroundColor: themeData.contenedorPrincipal,
+        }
+    })
+}
