c2281c70e7c2bea6ff49fea5ebc9db4c5889ef7b
MartinB04 <martin.bibiano4@outlook.com>
2024-11-12 18:00:18 -0600

    Optimize: Implementación de iconos IonIcons en BottomTabNavigator.
    [BottomTabNavigator.js]: Optimize.
    * Implementación de iconos de IonIcons en tabs de navegación.
    * Facilita el cambio de iconos simplemente cambiando el nombre en lugar de insertarlos manualmente en el proyecto.
    
    [App.js]: Change.
    * Ajuste de StatusBar al estilo oscuro para mejor visualización.

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Button, StyleSheet, Image, ScrollView } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigator/StackNavigator.js';
-
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
 
   return (
     <NavigationContainer>
+      <StatusBar style='dark' />
       <StackNavigator/>
     </NavigationContainer>
   );
--- src/navigator/BottomTabNavigator.js
+++ src/navigator/BottomTabNavigator.js
@@ -1,57 +1,73 @@
-import HoroscopeScreen from '../screens/HoroscopeScreen.js';
-import RecommendationsScreen from '../screens/RecommendationsScreen.js';
-import MoviesScreen from '../screens/MoviesScreen.js';
-import HomeSvg from '../svg_components/astrology-horoscope-svgrepo-com.svg';
-import MusicNoteSvg from '../svg_components/music-note-svgrepo-com.svg';
-import MovieReelSvg from '../svg_components/movie-play-button-2-svgrepo-com.svg';
-import StarSVG from '../svg_components/stars-svgrepo-com.svg';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../screens/HomeScreen.js';
+import HoroscopeScreen from "../screens/HoroscopeScreen.js";
+import RecommendationsScreen from "../screens/RecommendationsScreen.js";
+import MoviesScreen from "../screens/MoviesScreen.js";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen.js";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import React from "react";
 
 const Tab = createBottomTabNavigator();
 
 export function HomeTabs({ route }) {
-    // Extract the passed 'user' param
-    const { user } = route.params;
-    console.log(user)
-    return (
-      <Tab.Navigator>
-        <Tab.Screen
-          name="BirthChart"
-          component={HomeScreen}
-          initialParams={{ user }} // Pass 'user' to HomeScreen
-          options={{
-            tabBarIcon: ({ color, size }) => <HomeSvg fill={color} width={size} height={size}  />,
-            headerShown: false
-          }}
-        />
-        <Tab.Screen
-          name="Horoscope"
-          component={HoroscopeScreen}
-          initialParams={{ user }} // Pass 'user' to HoroscopeScreen
-          options={{
-            tabBarIcon: ({ color, size }) => <StarSVG fill={color} width={size} height={size} />,
-            headerShown: false
-          }}
-        />
-        <Tab.Screen
-          name="Songs"
-          component={RecommendationsScreen}
-          initialParams={{ user }} // Pass 'user' to RecommendationsScreen
-          options={{
-            tabBarIcon: ({ color, size }) => <MusicNoteSvg fill={color} width={size} height={size} />,
-            headerShown: false
-          }}
-        />
-        <Tab.Screen
-          name="Movies"
-          component={MoviesScreen}
-          initialParams={{ user }} // Pass 'user' to RecommendationsScreen
-          options={{
-            tabBarIcon: ({ color, size }) => <MovieReelSvg fill={color} width={size} height={size} />,
-            headerShown: false
-          }}
-        />
-      </Tab.Navigator>
-    );
-  }
+  // Extract the passed 'user' param
+  const { user } = route.params;
+  console.log(user);
+  return (
+    <Tab.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "BirthChart") {
+            iconName = focused ? "map" : "map-outline"; // Cambia el icono según si está enfocado o no
+          } else if (route.name === "Horoscope") {
+            iconName = focused ? "star" : "star-outline"; // Cambia el icono según si está enfocado o no
+          } else if (route.name === "Songs") {
+            iconName = focused ? "musical-notes" : "musical-notes-outline"; // Cambia el icono según si está enfocado o no
+          } else if (route.name === "Movies") {
+            iconName = focused ? "film" : "film-outline"; // Cambia el icono según si está enfocado o no
+          }
+
+          // Retorna el ícono correspondiente de Ionicons
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: /* themeData.texto */ "black", // Cambia el color del ícono cuando la pestaña está activa
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: { backgroundColor: /* themeData.fondo */ "white" },
+      })}
+    >
+      {/* <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} /> */}
+      <Tab.Screen
+        name="BirthChart"
+        component={HomeScreen}
+        initialParams={{ user }}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Horoscope"
+        component={HoroscopeScreen}
+        initialParams={{ user }} // Pass 'user' to HoroscopeScreen
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Songs"
+        component={RecommendationsScreen}
+        initialParams={{ user }} // Pass 'user' to RecommendationsScreen
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Movies"
+        component={MoviesScreen}
+        initialParams={{ user }} // Pass 'user' to RecommendationsScreen
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
