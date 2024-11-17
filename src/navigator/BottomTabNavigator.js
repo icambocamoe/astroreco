a954976ae05b4293f7a0ce568dd4e90278d4d023
MartinB04 <martin.bibiano4@outlook.com>
2024-11-17 14:45:59 -0600

    Fix: Problemas con depenencias
    * Problemas con algunas dependencias solucionados.
    * El bottomTabNavigator enlaza al MaterialTopTabNavigator.
    * El MaterialTopTabNavigator funciona con el emulador.
    * De momento hay errores al ejecutar la app con el dispositivo fisico.

--- src/navigator/BottomTabNavigator.js
+++ src/navigator/BottomTabNavigator.js
@@ -9,6 +9,7 @@ import { SettingsScreen } from "../screens/SettingsScreen.js";
 import { BirthChartScreen } from "../screens/BirthChartScreen.js";
 import { HomeScreen } from "../screens/HomeScreen.js";
 import { ThemeContext } from "../context/ThemeContext.js";
+import { MaterialTopTabNavigator } from "./MaterialTopTabNavigator.js";
 
 const Tab = createBottomTabNavigator();
 
@@ -16,7 +17,6 @@ export function HomeTabs({ route }) {
   // Extract the passed 'user' param
   const { user } = route.params;
   console.log(user);
-
 
   const context = useContext(ThemeContext); // Obtiene el contexto
   const themeData = context?.themeData; // Obtiene themeData del contexto
@@ -42,21 +42,20 @@ export function HomeTabs({ route }) {
             iconName = focused ? "film" : "film-outline"; // Cambia el icono según si está enfocado o no
           } else if (route.name === "Settings") {
             iconName = focused ? "settings" : "settings-outline"; // Cambia el icono según si está enfocado o no
-          }
-          else if (route.name === "HomeScreen") {
+          } else if (route.name === "HomeScreen") {
             iconName = focused ? "home" : "home-outline"; // Cambia el icono según si está enfocado o no
           }
 
           // Retorna el ícono correspondiente de Ionicons
           return <Ionicons name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: themeData.texto  /* "black" */, // Cambia el color del ícono cuando la pestaña está activa
+        tabBarActiveTintColor: themeData.texto /* "black" */, // Cambia el color del ícono cuando la pestaña está activa
         tabBarInactiveTintColor: "gray",
-        tabBarStyle: { backgroundColor: themeData.fondo,  /* "white" */ },
+        tabBarStyle: { backgroundColor: themeData.fondo /* "white" */ },
       })}
     >
       {/* <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} /> */}
-      
+
       <Tab.Screen
         name="HomeScreen"
         component={HomeScreen}
@@ -88,6 +87,14 @@ export function HomeTabs({ route }) {
       <Tab.Screen
         name="Movies"
         component={MoviesScreen}
+        initialParams={{ user }} // Pass 'user' to RecommendationsScreen
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Material"
+        component={MaterialTopTabNavigator}
         initialParams={{ user }} // Pass 'user' to RecommendationsScreen
         options={{
           headerShown: false,
